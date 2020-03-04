535b4b4833c96e456e9cd08883c60ddfe6e66c43
Ramon Guarnes <rag4@pdx.edu>
2020-03-03 18:16:02 -0800

    Created Project 5

---
+++ airline-web/src/test/java/edu/pdx/cs410J/rag4/AirlineServletTest.java
@@ -0,0 +1,60 @@
+package edu.pdx.cs410J.rag4;
+
+import org.junit.Test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.*;
+
+/**
+ * A unit test for the {@link AirlineServlet}.  It uses mockito to
+ * provide mock http requests and responses.
+ */
+public class AirlineServletTest {
+
+  @Test
+  public void initiallyServletContainsNoDictionaryEntries() throws ServletException, IOException {
+    AirlineServlet servlet = new AirlineServlet();
+
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    PrintWriter pw = mock(PrintWriter.class);
+
+    when(response.getWriter()).thenReturn(pw);
+
+    servlet.doGet(request, response);
+
+    int expectedWords = 0;
+    verify(pw).println(Messages.formatWordCount(expectedWords));
+    verify(response).setStatus(HttpServletResponse.SC_OK);
+  }
+
+  @Test
+  public void addOneWordToDictionary() throws ServletException, IOException {
+    AirlineServlet servlet = new AirlineServlet();
+
+    String word = "TEST WORD";
+    String definition = "TEST DEFINITION";
+
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    when(request.getParameter("word")).thenReturn(word);
+    when(request.getParameter("definition")).thenReturn(definition);
+
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    PrintWriter pw = mock(PrintWriter.class);
+
+    when(response.getWriter()).thenReturn(pw);
+
+    servlet.doPost(request, response);
+    verify(pw).println(Messages.definedWordAs(word, definition));
+    verify(response).setStatus(HttpServletResponse.SC_OK);
+
+    assertThat(servlet.getDefinition(word), equalTo(definition));
+  }
+}
